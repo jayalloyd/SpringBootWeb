9e8c5ca991c432b6826df5003324efbb93f9ca5f
jayalloyd <jayaranilloyd1983@gmail.com>
2024-06-19 18:10:24 +0530

    sending data to the controller portion learnt

--- src/main/WebApp/index.jsp
+++ src/main/WebApp/index.jsp
@@ -1,6 +1,16 @@
 <%@page language="java"%>
 <html>
+<head>My Calculator</head>
 <body >
-Hello World
+<form action="add">
+    <label for="num1">Enter the first text:</label>
+    <input type="text" id="num1" name="num1"><br/>
+
+    <label for="num2">Enter the second text:</label>
+    <input type="text" id="num2" name="num2">
+
+    <input type="submit" value="Submit">
+</form>
+
 </body>
 </html>
--- src/main/java/com/meenuscuisines/SpringBootWeb/HomeController.java
+++ src/main/java/com/meenuscuisines/SpringBootWeb/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,5 +16,14 @@ public class HomeController  {
      @RequestMapping("/")//annotation for mapping
     public String home(){
         return "index.jsp";
+    }
+  @RequestMapping("add")//path that is given in index jsp page form tag uses Dispatcher Servlet
+    public String add(HttpServletRequest req){//Now using servlet only...
+         int num1= Integer.parseInt(req.getParameter("num1"));
+      int num2= Integer.parseInt(req.getParameter("num2"));
+      int result=num1+num2;
+      System.out.println("Result added");
+
+      return "Result.jsp";
     }
 }
