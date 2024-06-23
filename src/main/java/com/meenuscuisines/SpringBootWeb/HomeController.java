cf4a9f29004fb043ff5a4eb8fbe8d4a08ff3f611
jayalloyd <jayaranilloyd1983@gmail.com>
2024-06-23 21:37:05 +0530

    request param used replacing httpservlet

--- src/main/java/com/meenuscuisines/SpringBootWeb/HomeController.java
+++ src/main/java/com/meenuscuisines/SpringBootWeb/HomeController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -19,17 +20,29 @@ public class HomeController  {
         return "index.jsp";
     }
   @RequestMapping("add")//path that is given in index jsp page form tag uses Dispatcher Servlet
-    public String add(HttpServletRequest req, HttpSession session){//Now using servlet only...
-         int num1= Integer.parseInt(req.getParameter("num1"));
-      int num2= Integer.parseInt(req.getParameter("num2"));
-      int result=num1+num2;
-      System.out.println("Result added");
+//    public String add(HttpServletRequest req, HttpSession session){//Now using servlet only...
+//         int num1= Integer.parseInt(req.getParameter("num1"));
+//      int num2= Integer.parseInt(req.getParameter("num2"));
+//      int result=num1+num2;
+//      System.out.println("Result added");
+//
+//     //when we call add,dispatcher servlet is calling Result.jsp,ie two different pages
+//      // using session of servlet we can display result in a page
+//      session.setAttribute("result",result);
+//      //in result.jsp using session we can display the result <%=session.getAttribute("result)%>
+//      //either JSTL Expression language ${result}
+//      return "Result.jsp";
+//    }
+    public String add(@RequestParam("num1")int num1, @RequestParam("num2")int num2, HttpSession session){//Now using request param only..removing HTTpServletrequest
 
-     //when we call add,dispatcher servlet is calling Result.jsp,ie two different pages
-      // using session of servlet we can display result in a page
-      session.setAttribute("result",result);
-      //in result.jsp using session we can display the result <%=session.getAttribute("result)%>
-      //either JSTL Expression language ${result}
-      return "Result.jsp";
+        int result=num1+num2;
+        System.out.println("Result added");
+
+        //when we call add,dispatcher servlet is calling Result.jsp,ie two different pages
+        // using session of servlet we can display result in a page
+        session.setAttribute("result",result);
+        //in result.jsp using session we can display the result <%=session.getAttribute("result)%>
+        //either JSTL Expression language ${result}
+        return "Result.jsp";
     }
 }
