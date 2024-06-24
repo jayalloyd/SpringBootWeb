100bd6a2bc1db346232023206bb478d605946062
jayalloyd <jayaranilloyd1983@gmail.com>
2024-06-24 12:51:25 +0530

    used view resolver instead of using .jsp extension in jsp files

--- src/main/java/com/meenuscuisines/SpringBootWeb/HomeController.java
+++ src/main/java/com/meenuscuisines/SpringBootWeb/HomeController.java
@@ -18,9 +18,12 @@ import java.io.PrintWriter;
 public class HomeController  {
      @RequestMapping("/")//annotation for mapping
     public String home(){
-        return "index.jsp";
-    }
-  @RequestMapping("add")//path that is given in index jsp page form tag uses Dispatcher Servlet
+        return "index";// index.jsp will be resolved to /WEB-INF/Views/index.jsp
+     }
+
+
+    //after moving .jsp files to views folder removed extension and in application properties file do some mapping using view resolver
+//  @RequestMapping("add")//path that is given in index jsp page form tag uses Dispatcher Servlet
 //    public String add(HttpServletRequest req, HttpSession session){//Now using servlet only...
 //         int num1= Integer.parseInt(req.getParameter("num1"));
 //      int num2= Integer.parseInt(req.getParameter("num2"));
@@ -46,15 +49,21 @@ public class HomeController  {
 //        //either JSTL Expression language ${result}
 //        return "Result.jsp";
 //    }
-    public String add(@RequestParam("num1")int num1, @RequestParam("num2")int num2, Model model){//Now using model object to get rid of HttpSession
-        int result=num1+num2;
-        System.out.println("Result added");
-
-        //when we call add,dispatcher servlet is calling Result.jsp,ie two different pages
-        // using session of servlet we can display result in a page
-       model.addAttribute("result",result);
-        //in result.jsp using session we can display the result <%=session.getAttribute("result)%>
-        //either JSTL Expression language ${result}
-        return "Result.jsp";
+//    public String add(@RequestParam("num1")int num1, @RequestParam("num2")int num2, Model model){//Now using model object to get rid of HttpSession
+//        int result=num1+num2;
+//        System.out.println("Result added");
+//
+//        //when we call add,dispatcher servlet is calling Result.jsp,ie two different pages
+//        // using session of servlet we can display result in a page
+//       model.addAttribute("result",result);
+//        //in result.jsp using session we can display the result <%=session.getAttribute("result)%>
+//        //either JSTL Expression language ${result}
+//        return "Result.jsp";
+//    }
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+        int result = num1 + num2;
+        model.addAttribute("result", result);
+        return "Result";  // Resolves to /WEbApp/Views/Result.jsp
     }
 }
